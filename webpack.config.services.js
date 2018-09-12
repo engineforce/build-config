a9f6a9e0f30dc6ec11fe7724047db37db9da8e18
Paul Li <paul.li@tigerspike.com>
2018-09-13 00:00:53 +1000

    Try to build

---
+++ webpack.config.services.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+function getBaseConfig(basePath) {
+  return {
+    entry: slsw.lib.entries,
+    target: 'node',
+    // externals: [nodeExternals()],
+    externals: ['aws-sdk', 'aws-xray-sdk-core'],
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+      // modules: [path.resolve(baseDir), "node_modules"],
+    },
+    output: {
+      libraryTarget: 'commonjs',
+      path: path.join(basePath, '.webpack'),
+      filename: '[name].js',
+      pathinfo: true,
+    },
+    module: {
+      rules: [
+        // { test: /\.ts(x?)$/, loader: 'ts-loader' },
+        { test: /\.js(x?)$/, loader: 'babel-loader' },
+      ],
+    },
+  };
+}
+
+module.exports = { getBaseConfig };
